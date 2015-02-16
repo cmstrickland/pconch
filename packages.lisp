561d9fb41f96ce4c120e3f74c4b856f208285406
Colin M. Strickland <cms@beatworm.co.uk>
2015-02-16 08:42:56 +0000

    Initial checkin
    Setup packages, simple API to start and stop web service

---
+++ packages.lisp
@@ -0,0 +1,6 @@
+(ql:quickload '(cl-who hunchentoot))
+(defpackage :pconch
+  (:use :cl)
+  (:export :app :setup))
+
+
---
+++ run.lisp
@@ -0,0 +1,15 @@
+(load "packages.lisp")
+(in-package pconch)
+
+(defun serve (&key port )
+  (setf acceptor (make-instance 'hunchentoot:easy-acceptor :port port)))
+
+(defun app (cmd) cmd)
+
+(defun setup (&key (port 2125) (root-prefix "/") (acceptor nil acceptor-bound-p))
+  (let ((acceptor (serve :port port)))
+    (setf (symbol-function 'app)
+          #'(lambda (cmd)
+              (cond ((equal :start cmd) (hunchentoot:start acceptor))
+                    ((equal :stop  cmd) (hunchentoot:stop  acceptor)))))))
+
