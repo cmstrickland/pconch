9befbefe5f4fcd24d794587e40e152797ef4b71e
Colin M. Strickland <cms@beatworm.co.uk>
2015-02-16 08:48:52 +0000

    Docstrings
    Added docstring for setup

--- run.lisp
+++ run.lisp
@@ -6,7 +6,9 @@
 
 (defun app (cmd) cmd)
 
+
 (defun setup (&key (port 2125) (root-prefix "/") (acceptor nil acceptor-bound-p))
+  "Create a default acceptor and bind pconch:app to a function that starts and stops it"
   (let ((acceptor (serve :port port)))
     (setf (symbol-function 'app)
           #'(lambda (cmd)
