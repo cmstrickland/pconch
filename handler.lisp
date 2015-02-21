0e7e1c7c50308fe35c582044ea67f0cda03612a0
Colin M. Strickland <cms@beatworm.co.uk>
2015-02-21 19:53:30 +0000

    Decompose request paths into a category -> topic pair
    Assumes that we always have a two tier hierarchy

--- handler.lisp
+++ handler.lisp
@@ -1,10 +1,16 @@
 (in-package :pconch)
 
 
+(defun uri-path (uri)
+  (cdr (puri:uri-parsed-path (puri:parse-uri uri))))
+
 (defun decode-path (req-uri)
-  (subseq (puri:uri-parsed-path (puri:parse-uri req-uri)) 1 3))
+  (let ((prefix-path (uri-path *prefix*))
+        (req-path (uri-path req-uri)))
+    (subseq (remove-prefix prefix-path req-path) 0 2)))
 
 (defun handler ()
-  (destructuring-bind (category item) (decode-path (hunchentoot:request-uri hunchentoot:*request*))
+  (destructuring-bind (category item)
+      (decode-path (hunchentoot:request-uri hunchentoot:*request*))
     (format nil "Looking up ~a in section ~a" item category)))
 
