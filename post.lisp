d5298f53af0bbe019c1cbbc3852b508498a6f66a
Colin M. Strickland <cms@beatworm.co.uk>
2015-02-28 14:38:18 +0000

    Construct a post object with read lines
    don't do anything much about parsing the data yet

--- post.lisp
+++ post.lisp
@@ -6,16 +6,23 @@
       (eq 0 (count-if #'alpha-char-p line))))
 
 (defun parse-header (line)
-  (format t "header ~a~%" line))
+  (format nil "header ~a~%" line))
 
 (defun add-raw-content  (line)
-  (format t "content ~a~%" line))
+  (format nil "content ~a~%" line))
 
 (defun read-post (open-file)
-  (loop for line = (read-line open-file nil :EOF)
-     with header = nil
-     until (eq line :EOF)
-     do (cond ((blank-line line) (if header (setf header nil) (setf header t)))
-              (header (parse-header line))
-              (t (add-raw-content line)))))
+  (let ((post (make-instance 'post)))
+    (loop for line = (read-line open-file nil :EOF)
+       with header = nil
+       until (eq line :EOF)
+       do (cond ((blank-line line) (if header (setf header nil) (setf header t)))
+                (header (setf (headers post) (push  (parse-header line) (headers post))))
+                (t (setf (content post) (concatenate 'string (content post) line))))) post))
+
+
+(defclass post ()
+  ((headers :accessor headers :initform nil)
+   (content :accessor content :initform nil)))
+
 
