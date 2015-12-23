6d464033450d4672bbbc227aaf05256d756a8d54
cms <cms@beatworm.co.uk>
2015-12-23 20:50:19 +0000

    reduce routes to a helper function
    pass it a list of sinatra style route expressions and function symbols,
    return a mapper object with the GET routes mapped onto the functions

--- routes.lisp
+++ routes.lisp
@@ -1,16 +1,5 @@
 (in-package :pconch)
 
-(defparameter *route-mapper* (myway:make-mapper))
-
-(defun build-index (params)
-  (format t "index ~a" (getf params :category)))
-
-(defun build-entry (params)
-  (format t "entry ~a" (getf params :topic)))
-
-(defun build-feed (params)
-  (format t "feed:~a" (getf params :category)))
-
 ;; (myway:connect *route-mapper* "/"  #'build-index)
 ;; (myway:connect *route-mapper* "/?:category?/*.rss" #'build-feed)
 ;; (myway:connect *route-mapper* "/:category/" #'build-index)
@@ -20,9 +9,3 @@
   (let ((mapper (myway:make-mapper)))
     (dolist (r routes mapper)
       (myway:connect mapper (first r) (symbol-function (cadr r))))))
-
-(defparameter *route-mapper*
-  (map-routes '(("/" build-index)
-                ("/?:category?/*.rss" build-feed)
-                ("/:category/" build-index)
-                ("/:category/:topic/?" build-entry))))
