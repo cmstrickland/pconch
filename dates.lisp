f8ec57496dcd63f62c82636c7e7328e95c1aa0a2
cms <cms@beatworm.co.uk>
2016-09-03 19:46:11 +0100

    Fix date vars
    I was binding the same var multiple times, which is naughty

--- dates.lisp
+++ dates.lisp
@@ -1,5 +1,5 @@
 (in-package :pconch)
 (defun formatted-date (n)
   "given a universal time return a formatted time string"
-  (multiple-value-bind (s m h d M Y d) (decode-universal-time n)
+  (multiple-value-bind (s mi h d M Y ) (decode-universal-time n)
     (list (format nil "~d-~2,'0d-~2,'0d" Y M d))))
