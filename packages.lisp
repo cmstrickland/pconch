0b97072afcacbb82b8e4aa0eeecba82bd22829c7
cms <cms@beatworm.co.uk>
2017-07-23 20:49:03 +0100

    adds cl-markdown
    use this as a markdown parser for allowing markdown format posts

--- packages.lisp
+++ packages.lisp
@@ -1,4 +1,4 @@
-(ql:quickload '(osicat cl-who hunchentoot puri lquery array-utils clss plump trivial-indent uiop myway cl-ppcre))
+(ql:quickload '(osicat cl-who hunchentoot puri lquery array-utils clss plump trivial-indent uiop myway cl-ppcre cl-markdown))
 (defpackage :pconch
   (:use :cl)
   (:export :app :setup :boot))
