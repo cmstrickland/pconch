260773533de8450fa8db4cc8fc46b91952f0c707
cms <cms@beatworm.co.uk>
2019-03-16 16:41:18 +0000

    last clean build on ARM

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ sbcl --load "/usr/share/cl-quicklisp/quicklisp.lisp" --eval '
 mkdir -p $QL_LOCAL
 mkdir -p $QL_LOCAL/pconch
 curl -L http://www.method-combination.net/lisp/files/ironclad.tar.gz  | (cd $QL_LOCAL ; tar xzvf - ) 
+curl -L https://common-lisp.net/project/cffi/releases/cffi_latest.tar.gz | (cd $QL_LOCAL ; tar xzvf - )
 #sed -e'/pushnew :ironclad-assembly/ s/^/;/g' -i $QL_LOCAL/ironclad-0.45/src/package.lisp 
-cp *asd *.lisp $QL_LOCAL/pconch/
 cp asdf.lisp $HOME
+cp *asd *.lisp $QL_LOCAL/pconch/
 cp sbcl-compile.lisp $HOME
 (cd $HOME ; sbcl --load sbcl-compile.lisp)
