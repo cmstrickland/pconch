92a1e19e97d953e746ff417447774a5567b406e8
cms <cms@beatworm.co.uk>
2020-07-08 08:02:11 +0100

    fix to dir-mtime
    uiop:directory-files has started complaining about my wildcard path.
    i was passing it merge "path" "*", but it actually is just wanting a
    directory maybe

--- util.lisp
+++ util.lisp
@@ -78,7 +78,7 @@ is a keyword and the cdr is whitespace trimmed"
                        #-sbcl(osicat-posix:stat-mtime (osicat-posix:stat f))
                        #+sbcl (sb-posix:stat-mtime (sb-posix:stat f))
                        )
-                     (uiop:directory-files (uiop:merge-pathnames* pathstring "*")))
+                     (uiop:directory-files pathstring))
              #'>)))
 
 
